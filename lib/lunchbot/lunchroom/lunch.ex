59451804aaef1c5b47305c57f31cfc4f47458548
Bartosz Markowski <nenros@gmail.com>
2019-07-31 10:01:39 +0200

    Update lunch.ex

--- lib/lunchbot/lunchroom/lunch.ex
+++ lib/lunchbot/lunchroom/lunch.ex
@@ -18,7 +18,7 @@ defmodule Lunchbot.Lunchroom.Lunch do
           company_image: Map.get(List.first(data), :company_image),
           dishes: map_dishes(data)
         })
-      {:ok, _} -> {:error, :no_lunch_choosen}
+      {:error, _} -> {:error, :no_lunch_choosen}
     end
   end
 
