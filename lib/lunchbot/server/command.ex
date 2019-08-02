2aeb16f0a5d5b42f1313a6a8bff74dd000e2d52c
Bartosz Markowski <bartosz.markowski@airhelp.com>
2019-08-02 10:52:21 +0200

    fix in response time

--- lib/lunchbot/server/command.ex
+++ lib/lunchbot/server/command.ex
@@ -1,7 +1,7 @@
 defmodule Lunchbot.Server.Command do
   import Plug.Conn, only: [send_resp: 3]
   def lunch(conn) do
-    Task.async(fn ()-> Lunchbot.Webhook.run_webhook(conn) end)
+    Task.start(Lunchbot.Webhook, :run_webhook, [conn])
     send_resp(conn, 200, "")
   end
 
