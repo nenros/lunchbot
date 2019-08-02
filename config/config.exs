567216eaa9879b57034284c42f35a528ad8ba2fb
Bartosz Markowski <nenros@gmail.com>
2019-08-02 20:23:45 +0200

    update logger config, add head route

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,11 @@ config :lunchbot, Lunchbot.Repo,
   # it can be overridden using the DATABASE_URL environment variable
   url: "ecto://kuoGfGzy:y_MiBPcpiDGsR2VHB_@localhost:6543/lunchbot?ssl=false&pool_size=10"
 
+if Mix.env() == :prod do
+  config :logger,
+    level: :info
+end
+
 if Mix.env() == :test do
   config :lunchbot, Lunchbot.Repo,
     # it can be overridden using the DATABASE_URL environment variable
--- lib/lunchbot/server/router.ex
+++ lib/lunchbot/server/router.ex
@@ -11,11 +11,13 @@ defmodule Lunchbot.Server.Router do
 
   plug(Plug.Logger)
   plug(Plug.Parsers, parsers: [:urlencoded, :multipart])
+  plug(Plug.Head)
   plug(:match)
   plug(:dispatch)
 
   alias Lunchbot.Server.Command
 
   get("/", do: send_resp(conn, 200, ""))
+
   post("webhook", do: Command.lunch(conn))
 end
