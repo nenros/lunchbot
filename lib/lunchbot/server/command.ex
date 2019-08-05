e08ccd4d623b56272888ff8fe3a6bc25bf441604
Bartosz Markowski <bartosz.markowski@airhelp.com>
2019-08-05 10:15:18 +0200

    Update command.ex

--- lib/lunchbot/server/command.ex
+++ lib/lunchbot/server/command.ex
@@ -1,8 +1,15 @@
 defmodule Lunchbot.Server.Command do
   import Plug.Conn, only: [send_resp: 3]
+  require ScoutApm.Tracing
 
   def lunch(conn) do
-    Task.start(Lunchbot.Webhook, :run_webhook, [conn])
+    Task.start(
+      fn ->
+        ScoutApm.Tracing.transaction(:background, "Webhook") do
+          Lunchbot.Webhook.run_webhook(conn)
+        end
+      end
+    )
     send_resp(conn, 200, "")
   end
 end
